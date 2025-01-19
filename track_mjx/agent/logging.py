72d608e6898e0822b823828a4a883b3c53e1c34a
Scott Yang <67733409+scott-yj-yang@users.noreply.github.com>
2025-01-18 17:02:55 -0800

    Implemented orbax checkpointing system in linen (#46)
    * change type hinting
    
    * ocp checkpoint partially implemented
    
    * implement orbax checkpoint system
    
    * save multiple modules
    
    * reduce preallocation
    
    * orbax explore notebook
    
    * black formatted
    
    * update configs
    
    * update rodent configs
    
    * update demo notebook
    
    * update logging
    
    * updates the config setup, and make the environ setup customizable
    
    * Delete track_mjx/io/checkpoint/ocp_ckpt.py
    
    don't need this
    
    -------

--- track_mjx/agent/logging.py
+++ track_mjx/agent/logging.py
@@ -5,6 +5,7 @@ import hydra
 from omegaconf import DictConfig, OmegaConf
 import uuid
 from typing import Callable, Any
+from types import ModuleType
 
 import functools
 import jax
@@ -20,12 +21,10 @@ from track_mjx.agent import custom_ppo_networks
 from track_mjx.agent import custom_losses
 from brax.io import model
 import numpy as np
-import pickle
-import warnings
 from jax import numpy as jp
 
 from track_mjx.environment import custom_wrappers
-from brax.envs.base import PipelineEnv
+from brax.envs.base import Env
 
 
 def log_metric_to_wandb(metric_name: str, data: jp.ndarray, title: str = ""):
@@ -73,8 +72,7 @@ def setup_training_logging(
     params: custom_losses.PPONetworkParams,
     rollout_key: jax.random.PRNGKey,
     cfg: DictConfig,
-    env: PipelineEnv,
-    wandb: Callable[[int, dict[str, jp.ndarray]], None],
+    env: Env,
     model_path: str,
 ) -> None:
     """Logs metrics and videos for a reinforcement learning training rollout.
@@ -101,11 +99,10 @@ def setup_training_logging(
     jit_reset = jax.jit(rollout_env.reset)
     jit_step = jax.jit(rollout_env.step)
 
-    os.makedirs(model_path, exist_ok=True)
-    model.save_params(f"{model_path}/{num_steps}", params)
     jit_inference_fn = jax.jit(make_policy(params, deterministic=True))
     rollout_key, reset_rng, act_rng = jax.random.split(rollout_key, 3)
 
+    # do a rollout on the saved model
     state = jit_reset(reset_rng)
 
     rollout = [state]
@@ -192,17 +189,37 @@ def setup_training_logging(
         / pair_render_xml_path
     )
 
-    spec = mujoco.MjSpec()
-    spec = spec.from_file(str(_XML_PATH))
+    if cfg.env_config.walker_name == "rodent":
+        # TODO: Make this ghost rendering walker agonist
+        root = mjcf_dm.from_path(_XML_PATH)
+        rescale.rescale_subtree(
+            root,
+            0.9 / 0.8,
+            0.9 / 0.8,
+        )
 
-    # in training scaled by this amount as well
-    for geom in spec.geoms:
-        if geom.size is not None:
-            geom.size *= walker_config.rescale_factor
-        if geom.pos is not None:
-            geom.pos *= walker_config.rescale_factor
+        mj_model = mjcf_dm.Physics.from_mjcf_model(root).model.ptr
+        mj_model.opt.solver = {
+            "cg": mujoco.mjtSolver.mjSOL_CG,
+            "newton": mujoco.mjtSolver.mjSOL_NEWTON,
+        }["cg"]
+        mj_model.opt.iterations = 6
+        mj_model.opt.ls_iterations = 6
+        mj_data = mujoco.MjData(mj_model)
+    elif cfg.env_config.walker_name == "flybody":
+        spec = mujoco.MjSpec()
+        spec = spec.from_file(str(_XML_PATH))
 
-    mj_model = spec.compile()
+        # in training scaled by this amount as well
+        for geom in spec.geoms:
+            if geom.size is not None:
+                geom.size *= walker_config.rescale_factor
+            if geom.pos is not None:
+                geom.pos *= walker_config.rescale_factor
+
+        mj_model = spec.compile()
+    else:
+        raise ValueError(f"Unknown walker_name: {cfg.env_config.walker_name}")
 
     mj_model.opt.solver = {
         "cg": mujoco.mjtSolver.mjSOL_CG,
@@ -234,9 +251,7 @@ def setup_training_logging(
     # render while stepping using mujoco
     video_path = f"{model_path}/{num_steps}.mp4"
 
-    with imageio.get_writer(
-        video_path, fps=env_config.render_fps
-    ) as video:
+    with imageio.get_writer(video_path, fps=int((1.0 / env.dt))) as video:
         for qpos1, qpos2 in zip(qposes_rollout, qposes_ref):
 
             # TODO: ValueError: could not broadcast input array from shape (148,) into shape (74,)
@@ -249,3 +264,143 @@ def setup_training_logging(
             video.append_data(pixels)
 
     wandb.log({"eval/rollout": wandb.Video(video_path, format="mp4")})
+
+
+
+def render_rollout(
+    num_steps: int,
+    make_policy: Callable[
+        [custom_losses.PPONetworkParams, bool],
+        Callable[
+            [jax.numpy.ndarray, jax.random.PRNGKey],
+            tuple[jax.numpy.ndarray, dict[str, Any]],
+        ],
+    ],
+    params: custom_losses.PPONetworkParams,
+    rollout_key: jax.random.PRNGKey,
+    cfg: DictConfig,
+    env: Env,
+    model_path: str,
+):
+    """
+    Rendering rollout to visualize the walker with the reference expert demonstration trajectory, without wandb logging.
+    This will save the rendered video to the <model_path>_<num_steps>.mp4, it will also return all the frames for notebook inline rendering.
+    """
+    ref_trak_config = cfg["reference_config"]
+    env_config = cfg["env_config"]
+    walker_config = cfg["walker_config"]
+
+    # Wrap the env in the brax autoreset and episode wrappers
+    rollout_env = custom_wrappers.RenderRolloutWrapperTracking(env)
+
+    # define the jit reset/step functions
+    jit_reset = jax.jit(rollout_env.reset)
+    jit_step = jax.jit(rollout_env.step)
+
+    jit_inference_fn = jax.jit(make_policy(params, deterministic=True))
+    rollout_key, reset_rng, act_rng = jax.random.split(rollout_key, 3)
+
+    # do a rollout on the saved model
+    state = jit_reset(reset_rng)
+
+    rollout = [state]
+    for i in range(int(ref_trak_config.clip_length * env._steps_for_cur_frame)):
+        _, act_rng = jax.random.split(act_rng)
+        obs = state.obs
+        ctrl, extras = jit_inference_fn(obs, act_rng)
+        state = jit_step(state, ctrl)
+        rollout.append(state)
+
+    # might include those reward term in the visual rendering
+    # pos_rewards = [state.metrics["pos_reward"] for state in rollout]
+    # endeff_rewards = [state.metrics["endeff_reward"] for state in rollout]
+    # quat_rewards = [state.metrics["quat_reward"] for state in rollout]
+    # angvel_rewards = [state.metrics["angvel_reward"] for state in rollout]
+    # bodypos_rewards = [state.metrics["bodypos_reward"] for state in rollout]
+    # joint_rewards = [state.metrics["joint_reward"] for state in rollout]
+    # summed_pos_distances = [state.info["summed_pos_distance"] for state in rollout]
+    # joint_distances = [state.info["joint_distance"] for state in rollout]
+    # torso_heights = [state.pipeline_state.xpos[env.walker._torso_idx][2] for state in rollout]
+
+    # Render the walker with the reference expert demonstration trajectory
+    qposes_rollout = np.array([state.pipeline_state.qpos for state in rollout])
+    ref_traj = rollout_env._get_reference_clip(rollout[0].info)
+    print(f"clip_id:{rollout[0].info}")
+    qposes_ref = np.repeat(
+        np.hstack([ref_traj.position, ref_traj.quaternion, ref_traj.joints]),
+        env._steps_for_cur_frame,
+        axis=0,
+    )
+
+    pair_render_xml_path = env.walker._pair_rendering_xml_path
+    _XML_PATH = Path(__file__).resolve().parent.parent / "environment" / "walker" / pair_render_xml_path
+
+    if cfg.env_config.walker_name == "rodent":
+        # TODO: Make this ghost rendering walker agonist
+        root = mjcf_dm.from_path(_XML_PATH)
+        rescale.rescale_subtree(
+            root,
+            0.9 / 0.8,
+            0.9 / 0.8,
+        )
+
+        mj_model = mjcf_dm.Physics.from_mjcf_model(root).model.ptr
+        mj_model.opt.solver = {
+            "cg": mujoco.mjtSolver.mjSOL_CG,
+            "newton": mujoco.mjtSolver.mjSOL_NEWTON,
+        }["cg"]
+        mj_model.opt.iterations = 6
+        mj_model.opt.ls_iterations = 6
+        mj_data = mujoco.MjData(mj_model)
+    elif cfg.env_config.walker_name == "flybody":
+        spec = mujoco.MjSpec()
+        spec = spec.from_file(str(_XML_PATH))
+
+        # in training scaled by this amount as well
+        for geom in spec.geoms:
+            if geom.size is not None:
+                geom.size *= walker_config.rescale_factor
+            if geom.pos is not None:
+                geom.pos *= walker_config.rescale_factor
+
+        mj_model = spec.compile()
+    else:
+        raise ValueError(f"Unknown walker_name: {cfg.env_config.walker_name}")
+
+    mj_model.opt.solver = {
+        "cg": mujoco.mjtSolver.mjSOL_CG,
+        "newton": mujoco.mjtSolver.mjSOL_NEWTON,
+    }["cg"]
+
+    mj_model.opt.iterations = 6
+    mj_model.opt.ls_iterations = 6
+    mj_data = mujoco.MjData(mj_model)
+
+    site_id = [mj_model.site(i).id for i in range(mj_model.nsite) if "-0" in mj_model.site(i).name]
+    for id in site_id:
+        mj_model.site(id).rgba = [1, 0, 0, 1]
+
+    # visual mujoco rendering
+    scene_option = mujoco.MjvOption()
+    scene_option.sitegroup[:] = [1, 1, 1, 1, 1, 0]
+    # scene_option.flags[mujoco.mjtVisFlag.mjVIS_CONTACTPOINT] = True
+    # scene_option.flags[mujoco.mjtVisFlag.mjVIS_CONTACTFORCE] = True
+
+    # save rendering and log to wandb
+    mujoco.mj_kinematics(mj_model, mj_data)
+    renderer = mujoco.Renderer(mj_model, height=512, width=512)
+
+    # render while stepping using mujoco
+    video_path = f"{model_path}/{num_steps}.mp4"
+    frames = []
+    with imageio.get_writer(video_path, fps=int((1.0 / env.dt))) as video:
+        for qpos1, qpos2 in zip(qposes_rollout, qposes_ref):
+
+            # TODO: ValueError: could not broadcast input array from shape (148,) into shape (74,)
+            mj_data.qpos = np.append(qpos1, qpos2)
+            mujoco.mj_forward(mj_model, mj_data)
+            renderer.update_scene(mj_data, camera=env_config.render_camera_name, scene_option=scene_option)
+            pixels = renderer.render()
+            frames.append(pixels)
+            video.append_data(pixels)
+    return frames

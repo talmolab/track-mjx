fa152da694f42c98f98db32d4ad91268a28a9316
Charles Zhang <charles.zhang216@gmail.com>
2025-02-19 14:07:55 -0500

    log kl weight

--- track_mjx/agent/ppo.py
+++ track_mjx/agent/ppo.py
@@ -33,6 +33,7 @@ from brax.training.types import Params
 from brax.training.types import PRNGKey
 from brax.v1 import envs as envs_v1
 import flax.training
+import wandb
 
 from track_mjx.agent import losses, ppo_networks, gradients
 from track_mjx.environment import wrappers
@@ -561,6 +562,16 @@ def train(
             )
             logging.info(metrics)
             progress_fn(current_step, metrics)
+
+            # Log kl schedule
+            if use_kl_schedule:
+                wandb.log(
+                    {
+                        "training/kl_weight": kl_schedule(it),
+                    },
+                    commit=False,
+                )
+
             policy_param = _unpmap(
                 (training_state.normalizer_params, training_state.params.policy)
             )

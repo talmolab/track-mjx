c0fa9c889edf94bf749f36f1538a41cc10826ad7
Charles Zhang <33401293+charles-zhng@users.noreply.github.com>
2025-05-21 14:55:47 -0400

    fix import, add deepmind rescale implementation (#93)
    * recsale fix
    
    * fix import

--- track_mjx/environment/walker/spec_utils.py
+++ track_mjx/environment/walker/spec_utils.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+
 def _scale_vec(vec: list[float] | np.ndarray, s: float) -> None:
     """Scale a vector in-place by a scalar.
 
@@ -14,6 +15,42 @@ def _scale_vec(vec: list[float] | np.ndarray, s: float) -> None:
     """
     for i in range(len(vec)):
         vec[i] *= s
+
+
+def dm_scale_spec(spec, scale):
+    scaled_spec = spec.copy()
+
+    # Traverse the kinematic tree, scaling all geoms
+    def scale_bodies(parent, scale=1.0):
+        body = parent.first_body()
+        while body:
+            if body.pos is not None:
+                body.pos = body.pos * scale
+            for geom in body.geoms:
+                geom.fromto = geom.fromto * scale
+                geom.size = geom.size * scale
+                if geom.pos is not None:
+                    geom.pos = geom.pos * scale
+            scale_bodies(body, scale)
+            body = parent.next_body(body)
+
+    # if scale_actuators:
+    # # scale gear
+    for actuator in scaled_spec.actuators:
+        # scale the actuator gear by (scale ** 2),
+        # this is because muscle force-generating capacity
+        # scales with the cross-sectional area of the muscle
+        actuator.gear = actuator.gear * scale * scale
+
+    # scale the z-position for all keypoints
+    for keypoint in scaled_spec.keys:
+        qpos = keypoint.qpos
+        qpos[2] = qpos[2] * scale
+        keypoint.qpos = qpos
+        keypoint.qpos[2] = keypoint.qpos[2] * scale
+
+    scale_bodies(scaled_spec.body("walker"), scale)
+    return scaled_spec
 
 
 def _scale_body_tree(body, s: float) -> None:

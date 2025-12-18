48f46cf8808b8b5ed7fca9fd8ab6a5b833a02335
scott-yj-yang <scottyang177@gmail.com>
2025-12-18 14:28:51 -0800

    update KL arg

--- track_mjx/train.py
+++ track_mjx/train.py
@@ -349,6 +349,7 @@ def main(cfg: DictConfig):
         num_resets_per_eval=cfg.train_setup.eval_every // cfg.train_setup.reset_every,
         episode_length=episode_length,
         kl_weight=cfg.network_config.kl_weight,
+        kl_prior_type=cfg.network_config.get("kl_prior_type", "ar1"),
         network_factory=network_factory,
         ckpt_mgr=ckpt_mgr,
         checkpoint_to_restore=cfg.train_setup.checkpoint_to_restore,
